8557509fe18164ef0f81d2cd8d5e34d7ce88d9ec
Eliezer Pujols <eliezerpujols@outlook.com>
2016-10-08 09:55:38 -0300

    Better Touch Effect For Captions

--- dist/js/mocassin.js
+++ dist/js/mocassin.js
@@ -1,79 +1,74 @@
 /*!
-   Mocassin CSS v1.3.0 |  https://wideckop.github.io/mocassin.css/
-   Licensed under the MIT license
+	Mocassin CSS v1.3.0 |  https://wideckop.github.io/mocassin.css/
+	Licensed under the MIT license
 
-   Copyright (c) 2016 Eliezer Pujols (Wideckop)
+	Copyright (c) 2016 Eliezer Pujols (Wideckop)
 */
 
 ( function( window ) {
+	// Modernizr TouchStart
+	if( Modernizr.touch ) {
+		function classReg( className ) {
+			return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+		}
 
-	// classie.js https://github.com/desandro/classie/blob/master/classie.js
-	// class helper functions from bonzo https://github.com/ded/bonzo
+		// classList support for class management
+		var hasClass, addClass, removeClass;
 
-	function classReg( className ) {
-		return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-	}
-
-	// classList support for class management
-	// altho to be fair, the api sucks because it won't accept multiple classes at once
-	var hasClass, addClass, removeClass;
-
-	if ( 'classList' in document.documentElement ) {
-		hasClass = function( elem, c ) {
-			return elem.classList.contains( c );
-		};
-		addClass = function( elem, c ) {
-			elem.classList.add( c );
-		};
-		removeClass = function( elem, c ) {
-			elem.classList.remove( c );
-		};
-	}
-	else {
-		hasClass = function( elem, c ) {
-			return classReg( c ).test( elem.className );
-		};
-		addClass = function( elem, c ) {
-			if ( !hasClass( elem, c ) ) {
+		if ( 'classList' in document.documentElement ) {
+			hasClass = function( elem, c ) {
+				return elem.classList.contains( c );
+			};
+			addClass = function( elem, c ) {
+				elem.classList.add( c );
+			};
+			removeClass = function( elem, c ) {
+				elem.classList.remove( c );
+			};
+		}
+		else {
+			hasClass = function( elem, c ) {
+				return classReg( c ).test( elem.className );
+			};
+			addClass = function( elem, c ) {
+				if ( !hasClass( elem, c ) ) {
 					elem.className = elem.className + ' ' + c;
-			}
+				}
+			};
+			removeClass = function( elem, c ) {
+				elem.className = elem.className.replace( classReg( c ), ' ' );
+			};
+		}
+
+		function toggleClass( elem, c ) {
+			var fn = hasClass( elem, c ) ? removeClass : addClass;
+			fn( elem, c );
+		}
+
+		var mocassin = {
+			hasClass: hasClass,
+			addClass: addClass,
+			removeClass: removeClass,
+			toggleClass: toggleClass
 		};
-		removeClass = function( elem, c ) {
-			elem.className = elem.className.replace( classReg( c ), ' ' );
-		};
-	}
 
-	function toggleClass( elem, c ) {
-		var fn = hasClass( elem, c ) ? removeClass : addClass;
-		fn( elem, c );
-	}
+		// transport
+		if ( typeof define === 'function' && define.amd ) {
+			// AMD
+			define( mocassin );
+		} else {
+			// browser global
+			window.mocassin = mocassin;
+		}
 
-	var classie = {
-		// full names
-		hasClass: hasClass,
-		addClass: addClass,
-		removeClass: removeClass,
-		toggleClass: toggleClass,
-		// short names
-		has: hasClass,
-		add: addClass,
-		remove: removeClass,
-		toggle: toggleClass
-	};
+		[].slice.call( document.querySelectorAll( '.mc-item' ) ).forEach( function( el, i ) {
+			el.querySelector( '.mc-item a' ).addEventListener( 'touchstart', function(e) {
+				e.stopPropagation();
+			}, false );
 
-	// transport
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( classie );
-	} else {
-		// browser global
-		window.classie = classie;
-	}
-
-	// For touch event on touch screens
-	[].slice.call( document.querySelectorAll( '.mc-item' ) ).forEach( function( el, i ) {
-		el.addEventListener( 'touchstart', function(e) {
-			classie.toggle( this, 'touch' );
+			el.addEventListener( 'touchstart', function(e) {
+				mocassin.toggleClass( this, 'touch' );
+			}, false );
 		});
-	});
+	}
 })( window );
--- gulpfile.js
+++ gulpfile.js
@@ -13,36 +13,22 @@ var rename = require('gulp-rename');
 // Path Config
 var configPath = {
 	inputCSS: 'src/*.scss',
-	outputCSS: 'dist/css/',
-	// Js Path
-	inputJS: 'dist/js/*.js',
-	outputJS: 'dist/js/'
+	outputCSS: 'dist/css/'
 }
 
 gulp.task('createCSS', function () {
    // Find main `.css` file from the `src/` folder
    return gulp.src(configPath.inputCSS)
-      // Run Sass on those files
-      .pipe(sass({outputStyle: 'expanded'}))
-      .pipe(autoprefixer({
-         browser: ['last 2 versions'],
-         cascade: false
-      }))
-      .pipe(gulp.dest(configPath.outputCSS))
+		// Run Sass on those files
+		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(autoprefixer({
+			browser: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest(configPath.outputCSS))
 
 		.pipe(cssmin())
 		.pipe(rename({suffix: '.min'}))
-      // Write the resulting CSS in the output folder
-		.pipe(gulp.dest(configPath.outputCSS))		
+		// Write the resulting CSS in the output folder
+		.pipe(gulp.dest(configPath.outputCSS))
 });
-
-// Minify JS File
-gulp.task('minifyJS', function () {
-   return gulp.src(configPath.inputJS)
-		.pipe(jsmin())
-      .pipe(rename({suffix: '.min'}))
-      // Write the resulting Minify JS in the output folder
-      .pipe(gulp.dest(configPath.outputJS))
-});
-
-
